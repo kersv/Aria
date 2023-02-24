56febe8c2c7be0259861804aa6e708d348665e94
kevin xie <simpbum@gmail.com>
2023-02-24 14:46:41 -0500

    added logo and declared app name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import appLogo from './assets/Aria.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Welcome to </h1>
+        <img src={appLogo} alt='Aria'/>
       </header>
     </div>
   );
