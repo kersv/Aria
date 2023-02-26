96ff71818d890b1651bb9ab615e3d668fb484a23
Michaeltessey1 <michaeltessey1@gmail.com>
2023-02-26 17:07:34 -0500

    manually made logo

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import AriaLogo from '../../assets/Aria.jpg'
 import './home.scss'
+import Logo from '../../components/logo/logo.js'
 
 const Home = () => {
     return (
         <div className='home'>
             
             <div className='home-container'>
-                <h1>Welcome to Aria</h1>
-                <img src={AriaLogo} alt='Aria Logo'/>
+                <h1>Welcome to</h1>
+                <Logo />
+                {/* <img src={AriaLogo} alt='Aria Logo'/> */}
             </div>
             <Outlet/>
         </div>
